2dd14e550944c94491c486bca514d48676694c3a
dnfbd <dnfbd@naver.com>
2025-10-16 13:03:40 +0900

    feat:RN 환경셋팅

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,29 @@
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+}) {
+  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#007AFF',
+        headerShown: true,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function TabOneScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl font-bold">MailPocket</Text>
+      <View className="my-8 h-px w-4/5 bg-gray-200" />
+    </View>
+  );
+}
